84afc598da8e83297f0a66a538f995f73222a3b0
Maxi-stack <85815496+Maxi-stack@users.noreply.github.com>
2021-08-08 17:56:21 +1000

    Done

---
+++ index.js
@@ -0,0 +1,15 @@
+  function introduction (Max) {
+    return `Hi, my name is ${Max}.`;
+  }
+
+  function introductionWithLanguage (Max, JavaScript) {
+    return `Hi, my name is ${Max} and I am learning to program in ${JavaScript}.`;
+  }
+
+  function introductionWithLanguage (Max, JavaScript) {
+    return `Hi, my name is ${Max} and I am learning to program in ${JavaScript}.`;
+  }
+
+  function introductionWithLanguageOptional (Max, lang  = "JavaScript") {
+    return `Hi, my name is ${Max} and I am learning to program in ${lang}.`;
+  }
